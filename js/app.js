0213264eda5fd40695eb646e79836bb7645cfeff
dvid <contacto@dvidsilva.com>
2014-01-27 11:11:07 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+
+
+
+// create the module and name it scotchApp
+var intrepidApp = angular.module('intrepidApp', ['ngRoute']);
+
+
+
+  // configure our routes
+intrepidApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+
+  // route for the home page
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller  : 'homeController'
+  })
+
+  // route for the about page
+  .when('/service-areas', {
+    templateUrl : 'pages/services.html',
+    controller  : 'serviceController'
+  })
+
+  // route for the contact page
+  .when('/who-we-are', {
+    templateUrl : 'pages/whoweare.html',
+    controller  : 'whoController'
+  })
+  .otherwise({redirectTo: '/'});
+});
+
+
+// create the controller and inject Angular's $scope
+intrepidApp.controller('mainController', function($scope, $location) {
+  if( $location.path() === '/'){
+    $scope.current = 'home';
+    $scope.banner = 'true';
+  }
+});
+
+intrepidApp.controller('homeController', function($scope) {
+  $scope.current = 'home';
+  $scope.banner = true;
+  $scope.carrousel = function(){
+    jQuery('#ca-container').contentcarousel();
+  };
+});
+
+
+
+intrepidApp.controller('serviceController', function($scope) {
+  $scope.current = 'service';
+  $scope.banner = false;
+  $scope.services = services;
+});
+
+intrepidApp.controller('whoController', function($scope) {
+  $scope.current = 'who';
+  $scope.banner = false;
+});
+
+
+
---
+++ js/services.js
@@ -0,0 +1,16 @@
+var services = [
+  {title: "Health Information Exchange / Interoperability",
+    text: "What we do: Data exchange and interoperability needs assessments and strategic planning; community and enterprise HIE management services; vendor selection and technology implementation guidance; stakeholder outreach and engagement; policy development focused on alignment with federal and state initiatives (e.g. state-based HIE infrastructures, Healtheway)."},
+  {title: "Population Health Data Management / Analytics",
+    text: "What we do: Support provider, government, and non-profit institutions in realizing their goals for population health management. We draw upon our health IT industry subject matter expertise and deep architectural systems design skillsets."},
+  {title: "Meaningful Use of Electronic Health Records (EHRs)",
+    text: "What we do: Support providers in meeting Meaningful Use requirements, especially new requirements in 2014 related to data exchange including Transitions of Care and:"},
+  {title: "Public Health Reporting ",
+    text: "What we do: Assessments of providers’ current capabilities and needs for the electronic submission of data; guidance on efficiently meeting local, state, and federal requirements including Meaningful Use; vendor selection; system integration/implementation."},
+  {title: "Patient Engagement and Access to Information",
+    text: "What we do: Assessments of providers’ current capabilities and needs; guidance on 2014 Meaningful Use requirements for stages 1 and 2; recommend technical approaches; facilitate effective patient engagement and data sharing; promote and configure low-cost options using personal health records and Direct."},
+  {title: "IT Privacy, Security, and Infrastructure Development",
+    text: "What we do: Guidance on the development and implementation of IT privacy and security policies, protocols, and technical infrastructure for large and small-scale EHR and/or HIE projects, such as HIPAA OMNIBUS rule compliance and HITECH Act data exchange security provisions; standards-driven solutions architecting for data exchange infrastructures."},
+  {title: "User Experience / Workflow Redesign",
+  text: "What we do: Evaluate and improve the use of health IT applications within a practice or healthcare organization; workflow redesign for an integrated user experience to maximize provider satisfaction and effectiveness."}
+];
