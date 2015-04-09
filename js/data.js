ab88acd30630d12b8cc548ff4242d1ccfb88cda5
dvid <contacto@dvidsilva.com>
2015-04-09 14:45:17 -0700

    made cahnges to the services data

--- js/data.js
+++ js/data.js
@@ -1,23 +1,37 @@
 var services = [
-{title: "Health Information Exchange / Interoperability",
-text: "<strong>What we do:</strong> Data exchange and interoperability strategic planning; vendor selection and technology implementation guidance; HIE management services; stakeholder outreach and engagement; policy development focused on alignment with federal and state initiatives (e.g. state-based HIE infrastructures, Healtheway)."},
-{title: "Population Health Data Management / Analytics",
-text: "<strong>What we do:</strong> Support provider, government, and non-profit institutions in realizing their goals for population health management. We draw upon our health IT industry subject matter expertise and deep architectural systems design skillsets."},
-{title: "Meaningful Use of Electronic Health Records (ehrs)",
-text: "<strong>What we do:</strong> Support providers in meeting Meaningful Use requirements, especially new requirements in 2014 related to data exchange including Transitions of Care and:"},
-{title: "Public Health Reporting ",
-text: "<strong>What we do:</strong> Assessments of providers’ current capabilities and needs for the electronic submission of data; guidance on efficiently meeting local, state, and federal requirements including Meaningful Use; vendor selection; system integration/implementation.",
+{title : "health information exchange / interoperability",
+text : "<strong>What we do:</strong> Data exchange and interoperability strategic planning; vendor selection and technology implementation guidance; HIE management services; stakeholder outreach and engagement; governance and policy development focused on alignment with federal and state initiatives."
+},
+{title : "population health data management / analytics",
+text : "<strong>What we do:</strong> Support providers, health systems, and community initiatives in realizing their goals for population health management. We draw upon our expertise in health IT, systems architecture design, and clinical data research networks."
+},
+{title : "Clinical Data Research Networks",
+text : "<strong>What We Do:</strong> Provide guidance to emerging clinical data research networks on governance, policy, and technology alignment. Facilitate partnerships beyond research-oriented academic medical centers to include health systems, payers, health information exchange organizations, and community clinics."
+},
+{title : "Data Strategies for Managed Care",
+text : "<strong>What We Do:</strong> Support health care organizations in developing and implementing the data infrastructure required to succeed in managed care and value-based payment environments. We advise on strategies and tactics for health data analytics, care coordination, and care management."
+},
+{title : "it privacy, security, and infrastructure development",
+text : "<strong>What we do:</strong> Guidance on the development and implementation of IT privacy and security policies, protocols, and technical infrastructure for large and small-scale EHR and/or HIE projects, such as HIPAA OMNIBUS rule compliance and HITECH Act data exchange security provisions; standards-driven solutions architecting for data exchange infrastructures."
+
+},
+{title : "meaningful use of electronic health records (ehrs)",
+text : "<strong>What we do:</strong> Support providers in meeting Meaningful Use requirements, especially Stage 2 requirements related to data exchange including Transitions of Care and:"
+},
+{title : "public health reporting",
+text : "<strong>What we do:</strong> Assessments of providers’ current capabilities and needs for the electronic submission of data; guidance on efficiently meeting local, state, and federal requirements including Meaningful Use; vendor selection; system integration/implementation.",
 indented: true
 },
-{title: "Patient Engagement and Access to Information",
-text: "<strong>What we do:</strong> Assessments of providers’ current capabilities and needs; guidance on 2014 Meaningful Use requirements for stages 1 and 2; recommend technical approaches; facilitate effective patient engagement and data sharing; promote and configure low-cost options using personal health records and Direct.",
+{title : "patient engagement and access to information",
+text : "<strong>What we do:</strong> Assessments of providers’ current capabilities and needs; guidance on 2014 Meaningful Use requirements for stages 1 and 2; recommend technical approaches; facilitate effective patient engagement and data sharing; promote and configure low-cost options using personal health records and Direct.",
 indented: true
-},
-{title: "IT Privacy, Security, and Infrastructure Development",
-text: "<strong>What we do:</strong> Guidance on the development and implementation of IT privacy and security policies, protocols, and technical infrastructure for large and small-scale EHR and/or HIE projects, such as HIPAA OMNIBUS rule compliance and HITECH Act data exchange security provisions; standards-driven solutions architecting for data exchange infrastructures."},
-{title: "User Experience / Workflow Redesign",
-text: "<strong>What we do:</strong> Evaluate and improve the use of health IT applications within a practice or healthcare organization; workflow redesign for an integrated user experience to maximize provider satisfaction and effectiveness."}
+}
 ];
+
+
+
+
+
 var clients2014 = [
 {
 name: "Axesson",
